d9eecdbc0bde3694bbd718a66f725c89b76aab8a
minbaby.zhang <minbaby.zhang@gmail.com>
2018-12-16 21:22:00 +0800

    hanmming

---
+++ php/hamming/hamming.php
@@ -0,0 +1,10 @@
+<?php
+
+function distance($a, $b)
+{
+   if (strlen($a) !== strlen($b)) {
+       throw new InvalidArgumentException("DNA strands must be of equal length.");
+   }
+
+   return count(array_diff_assoc(str_split($a), str_split($b)));
+}
