fb8669bc493e0134fbabe138b9e47ffc265b59ed
minbaby.zhang <zhangshaomin_1990@126.com>
2018-12-19 19:31:03 +0800

    add metadata.json && update giga

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -1,22 +1,6 @@
 <?php
-function from ($date)
+function from (\DateTime $date)
 {
-    return new Gigasecond($date);
-}
-
-class Gigasecond {
-    /**
-     * @var \DateTime
-     */
-    private $date;
-
-    public function __construct(\DateTime $date)
-    {
-        $this->date = $date;
-    }
-
-    public function format($format)
-    {
-        return $this->date->add(\DateInterval::createFromDateString("1000000000 seconds"))->format($format);
-    }
+    $dateNew = clone $date;
+    return $dateNew->add(\DateInterval::createFromDateString("1000000000 seconds"));
 }
