4e879ad4ce210a6d32d5a8ef84253aa7a6878921
霸气千秋 <zhangshaomin_1990@126.com>
2022-05-29 15:36:40 +0800

    charp bob 优化代码，对不同逻辑进行拆封方法

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,50 +1,36 @@
 using System;
-using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class Bob
 {
+    public static bool IsQuiet(string statement)
+        => string.IsNullOrWhiteSpace(statement);
+
+    private static bool IsYelled(string statement)
+        => statement.Any(Char.IsUpper) && statement.ToUpper() == statement;
+
+    private static bool IsYelledQuesition(string statement)
+        => IsYelled(statement) &&  IsQuesition(statement);
+
+    private static bool IsQuesition(string statement)
+        => statement.Trim().EndsWith("?");
+
     public static string Response(string statement)
     {
 
-        statement = statement.Trim();
-
-        if (statement.Length == 0)
+        switch (statement)
         {
-            return "Fine. Be that way!";
-
+            case string msg when IsYelledQuesition(statement):
+                return "Calm down, I know what I'm doing!";
+            case string msg when IsQuesition(statement):
+                return "Sure.";
+            case string msg when IsYelled(statement):
+                return "Whoa, chill out!";
+            case string msg when IsQuiet(statement):
+                return "Fine. Be that way!";
+            default:
+                return "Whatever.";
         }
-
-        if (statement == "WHAT'S GOING ON?")
-        {
-            return "Calm down, I know what I'm doing!";
-        }
-
-        if (statement.EndsWith("?"))
-        {
-            return "Sure.";
-        }
-
-        bool flag = false;
-        foreach (var item in new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' })
-        {
-            if (statement.Contains(item))
-            {
-                flag = false;
-                break;
-            }
-
-            if (statement.Contains(item, StringComparison.OrdinalIgnoreCase))
-            {
-                flag = true;
-            }
-        }
-
-        if (flag == true)
-        {
-            return "Whoa, chill out!";
-        }
-
-        return "Whatever.";
     }
 
 }
