38f70f6be43b391eedc2b326e158fe45925971fc
霸气千秋 <zhangshaomin_1990@126.com>
2022-06-08 14:42:41 +0800

    chsarp/bird-watcher

---
+++ csharp/bird-watcher/BirdWatcher.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+
+class BirdCount
+{
+    private int[] birdsPerDay;
+
+    public BirdCount(int[] birdsPerDay)
+    {
+        this.birdsPerDay = birdsPerDay;
+    }
+
+    public static int[] LastWeek()
+    {
+        return new int[] { 0, 2, 5, 3, 7, 8, 4 };
+    }
+
+    public int Today()
+    {
+        return birdsPerDay.Last();
+    }
+
+    public void IncrementTodaysCount()
+    {
+        birdsPerDay[birdsPerDay.Length - 1]++;
+    }
+
+    public bool HasDayWithoutBirds()
+    {
+        return birdsPerDay.Contains(0);
+    }
+
+    public int CountForFirstDays(int numberOfDays)
+    {
+        return birdsPerDay.Take(numberOfDays).Sum();
+    }
+
+    public int BusyDays()
+    {
+        return birdsPerDay.Count(f => f >= 5);
+    }
+}
