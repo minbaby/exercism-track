6e8ab5b19515b718f8914e623181da1bc01d410e
minbaby.zhang <zhangshaomin_1990@126.com>
2022-06-09 17:17:36 +0800

    csharp/hyper-optimized-telemetry

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -1,49 +1,76 @@
 using System;
+using System.Linq;
 
 public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        // ushort
-        // short
-        // int
-        // long
         byte _t = reading switch
         {
-            >= 4_294_967_296 and <= 9_223_372_036_854_775_807 => 256 - sizeof(long),
-            >= 2_147_483_648 and <= 4_294_967_295 => sizeof(uint),
-            >= 65_536 and <= 2_147_483_647 => 256 - sizeof(int),
-            >= 0 and <= 65_535 => sizeof(ushort),
-            >= -32_768 and <= -1 => 256 - sizeof(short),
-            >= -2_147_483_648 and <= -32_769 => sizeof(int),
-            >= -9_223_372_036_854_775_808 and <= -2_147_483_649 => sizeof(long),
+            >= 4_294_967_296 and <= 9_223_372_036_854_775_807 => 256 - sizeof(long), // 0xF8, -8
+            >= 2_147_483_648 and <= 4_294_967_295 => sizeof(uint), // 4
+            >= 65_536 and <= 2_147_483_647 => 256 - sizeof(int), // 0xFC, -4
+            >= 0 and <= 65_535 => sizeof(ushort), // 2
+            >= -32_768 and <= -1 => 256 - sizeof(short), // FE, -2
+            >= -2_147_483_648 and <= -32_769 => 256 - sizeof(int), // 0xFC -4
+            >= -9_223_372_036_854_775_808 and <= -2_147_483_649 => 256 - sizeof(long), // 0xF8 -8
         };
 
+        var index = 0;
         var arr = new byte[9];
-        arr[0] = _t;
+        arr[index++] = _t;
 
-        var x = reading;
-        var index = 1;
+        byte[] buffer = new byte[0];
 
-        if (x < 0)
+        if (reading >= 0)
         {
-            arr[index] = 0xff;
-            index++;
+            buffer = BitConverter.GetBytes(reading);
+        }
+        else
+        {
+            switch (arr[0])
+            {
+                case 0xFE:
+                    buffer = BitConverter.GetBytes((short)reading);
+                    break;
+                case 0xFC:
+                    buffer = BitConverter.GetBytes((int)reading);
+                    break;
+                case 0xF8:
+                    buffer = BitConverter.GetBytes((long)reading);
+                    break;
+            }
         }
 
-        while (x >= 0xff)
+        for (int i = 0; i < buffer.Length; i++)
         {
-            arr[index] = ((byte)(x % 0x100));
-            x /= 0x100;
-            index++;
+            arr[i + 1] = buffer[i];
         }
-        arr[index] = ((byte)x);
-
         return arr;
     }
 
     public static long FromBuffer(byte[] buffer)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
+        switch (buffer[0])
+        {
+            case 2:
+                return BitConverter.ToUInt16(buffer, 1);
+            case 4:
+                return BitConverter.ToUInt32(buffer, 1);
+            case 8:
+                return BitConverter.ToInt64(buffer, 1);
+            case 0xFE: // -2
+                buffer[0] = (byte) (256 - buffer[0]);
+                return BitConverter.ToInt16(buffer, 1);
+            case 0xFC:// -4
+                buffer[0] = (byte)(256 - buffer[0]);
+                return BitConverter.ToInt32(buffer, 1);
+            case 0xF8: // -8
+                buffer[0] = (byte)(256 - buffer[0]);
+                return BitConverter.ToInt64(buffer, 1);
+            default:
+                return 0;
+        }
+
     }
 }
