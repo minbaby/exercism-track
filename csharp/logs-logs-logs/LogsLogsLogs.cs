9f65120bdf08c34090357b72c7a1f6c3862d2cfe
霸气千秋 <zhangshaomin_1990@126.com>
2022-06-10 00:30:28 +0800

    csharp/logs-logs-logs

---
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+// TODO: define the 'LogLevel' enum
+enum LogLevel
+{
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+    Unknown = 0,
+}
+
+static class LogLine
+{
+    public static LogLevel ParseLogLevel(string logLine)
+    {
+        return logLine[1..4] switch
+        {
+            "TRC" => LogLevel.Trace,
+            "DBG" => LogLevel.Debug,
+            "INF" => LogLevel.Info,
+            "WRN" => LogLevel.Warning,
+            "ERR" => LogLevel.Error,
+            "FTL" => LogLevel.Fatal,
+            _ => LogLevel.Unknown,
+        };
+    }
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+    {
+        return $"{(int)logLevel}:{message}";
+    }
+}
