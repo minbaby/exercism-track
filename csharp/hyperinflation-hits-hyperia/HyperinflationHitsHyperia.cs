58892f44fba1e04485633b21c9eca8609207ca8e
霸气千秋 <zhangshaomin_1990@126.com>
2022-06-11 13:43:52 +0800

    csharp/hyperinflation-hits-hyperia

---
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -0,0 +1,35 @@
+using System;
+
+public static class CentralBank
+{
+    public static string DisplayDenomination(long @base, long multiplier)
+    {
+        try
+        {
+            return checked(@base * multiplier).ToString();
+        }
+        catch (OverflowException e)
+        {
+            return "*** Too Big ***";
+        }
+    }
+
+    public static string DisplayGDP(float @base, float multiplier)
+    {
+        var ret = @base * multiplier;
+
+        return float.IsInfinity(ret) ? "*** Too Big ***" : ret.ToString();
+    }
+
+    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+    {
+        try
+        {
+            return checked(salaryBase * multiplier).ToString();
+        }
+        catch (OverflowException e)
+        {
+            return "*** Much Too Big ***";
+        }
+    }
+}
