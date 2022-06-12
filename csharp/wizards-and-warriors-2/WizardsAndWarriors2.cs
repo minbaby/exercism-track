6e3520d23e55f00c889794a723e1ae17aeb23bda
霸气千秋 <zhangshaomin_1990@126.com>
2022-06-12 10:14:45 +0800

    csharp/wizards-and-warriors-2

---
+++ csharp/wizards-and-warriors-2/WizardsAndWarriors2.cs
@@ -0,0 +1,54 @@
+using System;
+
+static class GameMaster
+{
+    public static string Describe(Character character)
+    {
+        return $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
+    }
+
+    public static string Describe(Destination destination)
+    {
+        return $"You've arrived at {destination.Name}, which has {destination.Inhabitants} inhabitants.";
+    }
+
+    public static string Describe(TravelMethod travelMethod)
+    {
+        var type = travelMethod switch
+        {
+            TravelMethod.Horseback => "on",
+            TravelMethod.Walking => "by",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        return $"You're traveling to your destination {type} {travelMethod.ToString().ToLower()}.";
+    }
+
+    public static string Describe(Character character, Destination destination, TravelMethod travelMethod)
+    {
+        return $"{Describe(character)} {Describe(travelMethod)} {Describe(destination)}";
+    }
+
+    public static string Describe(Character character, Destination destination)
+    {
+        return Describe(character, destination, TravelMethod.Walking);
+    }
+}
+
+class Character
+{
+    public string Class { get; set; }
+    public int Level { get; set; }
+    public int HitPoints { get; set; }
+}
+
+class Destination
+{
+    public string Name { get; set; }
+    public int Inhabitants { get; set; }
+}
+
+enum TravelMethod
+{
+    Walking,
+    Horseback
+}
