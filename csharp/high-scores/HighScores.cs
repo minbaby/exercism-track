e122faac7ba4019158061085c0b3a3c1394fb93d
霸气千秋 <zhangshaomin_1990@126.com>
2022-08-26 17:41:08 +0800

    csharp/exercises/high-scores

---
+++ csharp/high-scores/HighScores.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores
+{
+
+    private List<int> scores;
+
+    public HighScores(List<int> list)
+    {
+        scores = list;
+    }
+
+    public List<int> Scores()
+    {
+        return scores;
+    }
+
+    public int Latest()
+    {
+        return scores.Last();
+    }
+
+    public int PersonalBest()
+    {
+        return scores.Max();
+    }
+
+    public List<int> PersonalTopThree()
+    {
+        return scores.OrderByDescending(f => f).Take(3).ToList();
+    }
+}
