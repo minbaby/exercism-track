5d2367e1229de8f2405f5b83270a85b77f04d861
霸气千秋 <zhangshaomin_1990@126.com>
2022-09-19 16:52:08 +0800

    csharp/exercises/variable-length-quantity

--- csharp/variable-length-quantity/VariableLengthQuantity.cs
+++ csharp/variable-length-quantity/VariableLengthQuantity.cs
@@ -3,15 +3,89 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 //
-// 先把 137 转换成二进制：10001001
-// 7 bit 一组，把二进制分开，不足的补 0 ，变成 0000001 0001001
+// 先把 137 转换成二进制：10001001 （uint 默认就可以当作二进制处理）
+// 7 bit 一组，把二进制分开，不足的补 0 ，变成 0000001 0001001，（其实我可以每次循环处理7bit，这样就不用进行分组了）
 // 把最低的7位拿出来，在最高位补0表示最后一位，变成 0000 1001，这个作为最低位，放在最后边。
 // 在其他组的最高位补 1 ，表示没有结束，后面跟着还有数据。在这里就是 1000 0001
 // 拼在一起，就变成了 1000 0001 0000 1001 .
+// ~~~ 上边这个方法是核心思路，具体实现有很多，刚开始的时候我就是用的最笨的方法做的
 // 
 public static class VariableLengthQuantity
 {
     public static uint[] Encode(uint[] numbers)
+    {
+        var ret = new List<uint>();
+        foreach (var b in numbers)
+        {
+            var index = 0u;
+
+            var tmpList = new List<uint>();
+
+            var _b = b;
+            while (true)
+            {
+                index++;
+                var _tmp = _b & 0b_0111_1111;
+
+                _b >>= 7;
+
+                if (index != 1)
+                {
+                    // 不是最后一个
+                    _tmp |= 0b_1000_0000;
+                }
+
+                tmpList.Add(_tmp);
+
+
+                if (_b == 0)
+                {
+                    break;
+                }
+            }
+
+            tmpList.Reverse();
+            ret.AddRange(tmpList);
+        }
+        return ret.ToArray();
+    }
+    public static uint[] Decode(uint[] bytes)
+    {
+        var ret = new List<uint>();
+        var num = 0u;
+
+        var hasDone = false;
+        foreach (var b in bytes)
+        {
+            var _b = b;
+
+            var _tmp = _b & 0b_1111_1111;
+            var _value = _tmp & 0b_0111_1111;
+
+            if ((_tmp & 0b_1000_0000) == 0b_1000_0000)
+            {
+                hasDone = false;
+                // 没有结束
+                num = (num << 7) | _value;
+            }
+            else
+            {
+                hasDone = true;
+                // 结束了，我是最后一个
+                num = (num << 7) | _value;
+                ret.Add(num);
+                num = 0u;
+            }
+        }
+
+        if (!hasDone)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return ret.ToArray();
+    }
+    public static uint[] Encode1(uint[] numbers)
     {
         var ret = Array.Empty<uint>();
         foreach (var item in numbers)
@@ -51,8 +125,7 @@ public static class VariableLengthQuantity
 
         return ret;
     }
-
-    public static uint[] Decode(uint[] bytes)
+    public static uint[] Decode1(uint[] bytes)
     {
         var doneFlag = false;
         var ret = new List<uint>(bytes.Length);
@@ -86,7 +159,6 @@ public static class VariableLengthQuantity
 
         return ret.ToArray();
     }
-
     public static uint BitArrayToUint(uint[] data)
     {
         uint result = 0;
