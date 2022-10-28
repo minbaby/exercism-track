4f900f6620ce998cc4b62dfecc468971c1f7963c
霸气千秋 <zhangshaomin_1990@126.com>
2022-10-28 17:05:46 +0800

    csharp/exercises/darts

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,15 @@
+using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        return Math.Sqrt(x * x + y * y) switch
+        {
+            <= 1 => 10,
+            <= 5 => 5,
+            <= 10 => 1,
+            _ => 0,
+        };
+    }
+}
